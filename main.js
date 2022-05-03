780e23f70cb646b9a4470c9810985b916258abb8
Ian Cass <icass@akamai.com>
2022-05-03 10:22:51 +0100

    Work in progress

--- main.js
+++ main.js
@@ -1,13 +1,93 @@
-import { httpRequest } from 'http-request';
+import { l as lib, _ as __awaiter } from './vendors.js';
+import { TextDecoderStream, TextEncoderStream } from 'text-encode-transform';
 import { createResponse } from 'create-response';
+import { httpRequest } from 'http-request';
+import { logger } from 'log';
 import { TransformStream } from 'streams';
+
+class HTMLRewriterStream extends TransformStream {
+    constructor(handlers, ...args) {
+        const rewriter = new lib();
+        for (const key of Object.keys(handlers)) {
+            rewriter.on(key, (...args) => handlers[key](rewriter, ...args));
+        }
+        super({
+            start(controller) {
+                rewriter.on("data", chunk => controller.enqueue(chunk));
+                rewriter.on("close", () => controller.terminate());
+            },
+            transform(chunk) {
+                rewriter.write(chunk);
+            },
+            flush(controller) {
+                rewriter.end();
+            }
+        }, ...args);
+    }
+}
+
+class BodyOnly extends HTMLRewriterStream {
+    constructor() {
+	this.emit = 0;
+        super({
+            startTag: (emitter, startTag, raw) => {
+                if (startTag.tagName.equals('body')) {
+			this.emit = 1;
+                }
+                if (this.emit) {
+                   emitter.emitRaw(raw);
+		}
+            },
+            text(emitter, text, raw) {
+                if (this.emit) {
+                   emitter.emitRaw(raw);
+		}
+            },
+            endTag: (emitter, endTag, raw) => {
+                if (this.emit) {
+                	emitter.emitRaw(raw);
+		}
+            },
+        });
+    }
+}
+
+class HeadOnly extends HTMLRewriterStream {
+    constructor() {
+	this.emit = 1;
+        super({
+            startTag: (emitter, startTag, raw) => {
+                if (this.emit) {
+                   emitter.emitRaw(raw);
+		}
+            },
+            text(emitter, text, raw) {
+                if (this.emit) {
+                   emitter.emitRaw(raw);
+		}
+            },
+            endTag: (emitter, endTag, raw) => {
+                if (this.emit) {
+                	emitter.emitRaw(raw);
+		}
+                if (endTag.tagName.equals('head')) {
+			this.emit = 0;
+                }
+            },
+        });
+    }
+}
 
 function concatenateReadables(promiseOne, promiseTwo ) {
   
   let outStream = new TransformStream();
   promiseOne.then(
       response => { 
-	response.body.pipeTo(outStream.writable, { preventClose: true })
+          response.body
+            .pipeThrough(new TextDecoderStream())
+            .pipeThrough(new HeadOnly())
+            .pipeThrough(new TextEncoderStream())
+	    .pipeTo(outStream.writable, { preventClose: true })
       },
       reason => {
 	return Promise.all([
@@ -18,7 +98,11 @@ function concatenateReadables(promiseOne, promiseTwo ) {
   ).then(() => {
 	    promiseTwo.then(
 	      response => { 
-		response.body.pipeTo(outStream.writable, { preventClose: false })
+                response.body
+                    .pipeThrough(new TextDecoderStream())
+                    .pipeThrough(new BodyOnly())
+                    .pipeThrough(new TextEncoderStream())
+		    .pipeTo(outStream.writable, { preventClose: false })
 	      },
 	      reason => {
 		return Promise.all([
@@ -32,10 +116,13 @@ function concatenateReadables(promiseOne, promiseTwo ) {
   return outStream.readable;
 }
 
-export function responseProvider (request) {
+export function responseProvider(request) {
 
-    let promiseOne = httpRequest(`${request.scheme}://${request.host}/head${request.url}`)
-    let promiseTwo = httpRequest(`${request.scheme}://${request.host}/body${request.url}`)
+    let options = {}
+    options.headers = { "X-NO-ESI": "true" }
+
+    let promiseOne = httpRequest(`${request.scheme}://${request.host}/${request.url}`, options)
+    let promiseTwo = httpRequest(`${request.scheme}://${request.host}/${request.url}`)
 
     return Promise.resolve(
 	createResponse(
@@ -47,3 +134,4 @@ export function responseProvider (request) {
 	)
     );
 }
+
